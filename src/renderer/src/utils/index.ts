b2a352252c2a2d695bf842f44c16a6ef8b30584e
wz <wz@none.com>
2025-06-10 17:24:46 +0800

    t

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -1,4 +1,12 @@
 import { cloneDeep } from 'es-toolkit'
+import { NotificationType } from 'naive-ui'
 
 /**获取原生对象，避免与electron通信时错误 */
 export const toDeepRaw: <T>(v: T) => T = (v) => cloneDeep(v)
+
+export const notify = (msg: string, type: NotificationType = 'info') => {
+  window.$notification[type]({
+    content: msg,
+    duration: 2000
+  })
+}
